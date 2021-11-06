62a35c61a2df9fd2e036a626d543ebb5ef73aa08
trent <trent.gill@gmail.com>
2021-11-06 00:38:21 -0400

    quantizer should work now for basic 12TET usage

--- lib/quantize.lua
+++ lib/quantize.lua
@@ -1,35 +1,35 @@
-function quantize( volt, scale, divs, range )
-    -- defaults
-    chrom = {0,1,2,3,4,5,6,7,8,9,10,11}
-    if scale == nil then scale = chrom end
-    local notes = #scale
-    if notes == 0 then
-        scale = chrom
-        notes = #scale
+-- expect note_list as a set of 12Tet numbers
+-- output as a voltage-scaled CV
+-- TOOD ji support (as 'out_oct' param?)
+function quantizer(note_list, in_oct, out_oct)
+    in_oct, out_oct = in_oct or 12, out_oct or 1
+    local len = #note_list
+    local mul = out_oct/in_oct
+    local normal = len/in_oct
+
+    -- this function will take a 12Tet note num, and output a quantized note/voltage + octave
+    return function(n)
+        -- quantize n to the closured params above
+        n = n + 0.5 -- centre the window. TODO in_oct sensitive
+        local rem = n % in_oct 
+        local oct = n - rem -- increments in steps of in_oct 
+
+        return mul*note_list[math.floor(rem * normal) + 1], mul*oct
     end
-    divs,range = divs  or 12
-               , range or 1.0
-
-    -- separate note & octave
-    local wrap = 0
-    if type(divs) == 'string' then -- assume just intonation
-    else
-        while volt > range do
-            
-        end
-    end
-
-    -- calc index into table
-    local ix = 1+math.floor(volt/range * #scale)
-
-    -- scale to voltage
-    local v = 0
-    if type(divs) == 'string' then -- assume just intonation
-        v = math.log(scale[ix]) / math.log(2)
-    else
-        v = scale[ix] / divs
-    end
-
-    return v * range
-    -- reapply octave
 end
+
+-- example usage
+--[[
+-- quantize a 12TET style note into a voltage output
+q1 = quantizer{0,2,4,6,7,9,11}
+ii.wsyn.play_note(q1(math.random()*12), 2) -- play a random note in the scale
+
+local n, o = q1(math.random()*12*3) -- bigger range, save octave too
+ii.wsyn.play_note(n+o, 2) -- play a random note in scale, over 3 octaves
+
+s2 = quantizer{0,12,4,2,-5,6,11} -- the octave can be rearranged & extend the input range
+counter = 1
+-- this timeline will play through 3 octaves of sequence
+tl.loop{2, function() ii.wsyn.play_note(q1(counter % 36), 2); counter = counter + 1 end}
+
+]]
